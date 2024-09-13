09839502d1970d1a3fc25ecc253628702c786ec4
YuriMel0 <meloy316@gmail.com>
2024-09-13 13:04:33 -0300

    adicionando correção para caminho de diretorio de dados

--- src/spotify.py
+++ src/spotify.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
 
 st.set_page_config(
     layout="wide",
     page_title="Spotify"
 )
-df = pd.read_csv("../data/spotify_tracks.csv")
+
+base_dir = Path(__file__).resolve().parents[1]
+db_path = base_dir / 'data' / 'spotify_tracks.csv'
+df = pd.read_csv(db_path)
+
 df.set_index("name", inplace=True)
 
 artists = df["artists"].value_counts().index
